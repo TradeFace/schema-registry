0d06d069ff957e2ef0f7ff9ce32a58a00cdd5a62
Niels Berkers <niels.berkers@tradeface.nl>
2023-03-13 02:59:08 +0100

    feat: json-schema to avro

--- cmd/conv/main.go
+++ cmd/conv/main.go
@@ -3,65 +3,136 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"reflect"
-	"strings"
 )
 
-type SchemaObject struct {
-	Type          interface{}
-	Enum          []interface{}
-	Const         interface{}
-	Id            string
-	Schema        string
-	Title         string
-	Description   string
-	Properties    map[string]interface{}
-	Required      []string
-	MaxProperties uint64
-	MinProperties uint64
+type JSONSchema struct {
+	Type                 string                 `json:"type,omitempty"`
+	Items                *JSONSchema            `json:"items,omitempty"`
+	Properties           map[string]*JSONSchema `json:"properties,omitempty"`
+	Required             []string               `json:"required,omitempty"`
+	AdditionalProperties *JSONSchema            `json:"additionalProperties,omitempty"`
+	Definitions          map[string]*JSONSchema `json:"definitions,omitempty"`
+	Enum                 []interface{}          `json:"enum,omitempty"`
+	Format               string                 `json:"format,omitempty"`
+	Pattern              string                 `json:"pattern,omitempty"`
+	Maximum              *float64               `json:"maximum,omitempty"`
+	ExclusiveMaximum     *float64               `json:"exclusiveMaximum,omitempty"`
+	Minimum              *float64               `json:"minimum,omitempty"`
+	ExclusiveMinimum     *float64               `json:"exclusiveMinimum,omitempty"`
+	MaxLength            *int                   `json:"maxLength,omitempty"`
+	MinLength            *int                   `json:"minLength,omitempty"`
+	MultipleOf           *float64               `json:"multipleOf,omitempty"`
+	MaxItems             *int                   `json:"maxItems,omitempty"`
+	MinItems             *int                   `json:"minItems,omitempty"`
+	UniqueItems          bool                   `json:"uniqueItems,omitempty"`
+	Ref                  string                 `json:"$ref,omitempty"`
+	OneOf                []*JSONSchema          `json:"oneOf,omitempty"`
+	AnyOf                []*JSONSchema          `json:"anyOf,omitempty"`
+	AllOf                []*JSONSchema          `json:"allOf,omitempty"`
+	Not                  *JSONSchema            `json:"not,omitempty"`
+	Title                string                 `json:"title,omitempty"`
+	Description          string                 `json:"description,omitempty"`
+	Default              interface{}            `json:"default,omitempty"`
+	AdditionalItems      *JSONSchema            `json:"additionalItems,omitempty"`
+	ReadOnly             bool                   `json:"readOnly,omitempty"`
+	WriteOnly            bool                   `json:"writeOnly,omitempty"`
+	Examples             []interface{}          `json:"examples,omitempty"`
+	If                   *JSONSchema            `json:"if,omitempty"`
+	Then                 *JSONSchema            `json:"then,omitempty"`
+	Else                 *JSONSchema            `json:"else,omitempty"`
+	DependentSchemas     map[string]*JSONSchema `json:"dependentSchemas,omitempty"`
+	DependentRequired    map[string][]string    `json:"dependentRequired,omitempty"`
+	Contains             *JSONSchema            `json:"contains,omitempty"`
+	PropertyNames        *JSONSchema            `json:"propertyNames,omitempty"`
+	Anchor               string                 `json:"$anchor,omitempty"`
+	Merge                bool                   `json:"$merge,omitempty"`
+	RecursiveRef         bool                   `json:"$recursiveRef,omitempty"`
+	RecursiveAnchor      bool                   `json:"$recursiveAnchor,omitempty"`
+	DynamicRef           *JSONSchema            `json:"$dynamicRef,omitempty"`
+	Vocabulary           map[string]*JSONSchema `json:"$vocabulary,omitempty"`
+	Fluent               bool                   `json:"$fluent,omitempty"`
+	Comment              string                 `json:"$comment,omitempty"`
+	RefScope             string                 `json:"$refScope,omitempty"`
+	Extension            map[string]interface{} `json:"-"`
 }
 
-type SchemaArray struct {
-	Type        interface{}
-	Enum        []interface{}
-	Const       interface{}
-	Description string
-	Items       *SchemaArrayItems
-	MinItems    uint64
-	MaxItems    uint64
-	MaxContains uint64
-	MinContains uint64
-	UniqueItems bool
+type AvroSchema interface{}
+
+type PrimitiveType string
+
+const (
+	NullType    PrimitiveType = "null"
+	BooleanType PrimitiveType = "boolean"
+	IntType     PrimitiveType = "int"
+	LongType    PrimitiveType = "long"
+	FloatType   PrimitiveType = "float"
+	DoubleType  PrimitiveType = "double"
+	BytesType   PrimitiveType = "bytes"
+	StringType  PrimitiveType = "string"
+)
+
+type RecordField struct {
+	Name         string      `json:"name"`
+	Type         AvroSchema  `json:"type"`
+	DefaultValue interface{} `json:"default,omitempty"`
+	Doc          string      `json:"doc,omitempty"`
 }
 
-type SchemaArrayItems struct {
-	Type string
+type RecordType struct {
+	Type      string        `json:"type"`
+	Name      string        `json:"name"`
+	Namespace string        `json:"namespace,omitempty"`
+	Aliases   []string      `json:"aliases,omitempty"`
+	Doc       string        `json:"doc,omitempty"`
+	Fields    []RecordField `json:"fields"`
 }
 
-type SchemaNumber struct {
-	Type             interface{}
-	Enum             []interface{}
-	Const            interface{}
-	Description      string
-	MultipleOf       float64
-	Minimum          float64
-	ExclusiveMinimum float64
-	Maximum          float64
-	ExclusiveMaximum float64
+type EnumSymbol string
+
+type EnumType struct {
+	Type    string       `json:"type"`
+	Name    string       `json:"name"`
+	Doc     string       `json:"doc,omitempty"`
+	Symbols []EnumSymbol `json:"symbols"`
 }
 
-type SchemaString struct {
-	Type        interface{}
-	Enum        []interface{}
-	Const       interface{}
-	Description string
-	Pattern     string
-	MaxLength   uint64
-	MinLength   uint64
+type ArrayType struct {
+	Type  string     `json:"type"`
+	Items AvroSchema `json:"items"`
 }
+
+type MapType struct {
+	Type  string     `json:"type"`
+	Items AvroSchema `json:"values"`
+}
+
+type UnionType struct {
+	Types []AvroSchema `json:"type"`
+}
+
+type FixedType struct {
+	Type      string `json:"type"`
+	Name      string `json:"name"`
+	Namespace string `json:"namespace,omitempty"`
+	Size      int    `json:"size"`
+}
+
+type NameType struct {
+	Type      string `json:"type"`
+	Name      string `json:"name"`
+	Namespace string `json:"namespace,omitempty"`
+}
+
+type AliasesType struct {
+	Type    string   `json:"type"`
+	Name    string   `json:"name"`
+	Aliases []string `json:"aliases"`
+}
+
+type AnyType interface{}
 
 func main() {
-	schemaStr := `
+	schemaJSON := `
 	{
 		"$schema": "https://json-schema.org/draft/2020-12/schema",
 		"$id": "https://example.com/product.schema.json",
@@ -113,332 +184,146 @@ func main() {
 			"required": [ "length", "width", "height" ]
 		  }
 		},
-		"required": [ "productId", "productName", "price" ]
+		"required": [ "productId",  "price" ]
 	  }
 	  `
-
-	schemaMap := make(map[string]interface{})
-	if err := json.Unmarshal([]byte(schemaStr), &schemaMap); err != nil {
-		panic(err)
-	}
-	// TODO: use populate functions
-	schemaObject := SchemaObject{
-		Id:          schemaMap["$id"].(string),
-		Schema:      schemaMap["$schema"].(string),
-		Description: schemaMap["description"].(string),
-		Title:       schemaMap["title"].(string),
-		Properties:  schemaMap["properties"].(map[string]interface{}),
-		Required:    toStringSlice(schemaMap["required"].([]interface{})),
-	}
-	walkProperties(schemaObject.Properties, &schemaObject)
-
-	fmt.Println(schemaObject)
-	fmt.Println(displaySchema(&schemaObject, 0))
-
-	avrox, err := buildAvroSchema(&schemaObject)
+	var schema JSONSchema
+	err := json.Unmarshal([]byte(schemaJSON), &schema)
 	if err != nil {
+		fmt.Println("Error unmarshaling JSON schema:", err)
 		return
 	}
-	data, err := json.Marshal(avrox)
+
+	// fmt.Printf("%+v\n", schema)
+
+	res, err := jsonSchemaToAvroSchema([]byte(schemaJSON))
 	if err != nil {
+		fmt.Println("Error jsonSchemaToAvroSchema:", err)
 		return
 	}
-	fmt.Println(string(data))
+	// fmt.Println(res)
+
+	// Convert the result to a JSON string
+	resJSON, err := json.Marshal(res)
+	if err != nil {
+		fmt.Println("Error marshaling to JSON:", err)
+		return
+	}
+
+	// Print the JSON string
+	fmt.Println(string(resJSON))
+
 }
 
-func displaySchema(schema *SchemaObject, indent int) string {
-	var b strings.Builder
-
-	// write the current schema object's title and description
-	b.WriteString(strings.Repeat(" ", indent))
-	b.WriteString(schema.Title)
-	if schema.Description != "" {
-		b.WriteString(fmt.Sprintf(" (%s)", schema.Description))
+func walkSchema(schema *JSONSchema, recordName string) AvroSchema {
+	switch schema.Type {
+	case "null":
+		return NullType
+	case "boolean":
+		return BooleanType
+	case "integer":
+		if schema.Format == "int64" {
+			return LongType
+		} else {
+			return IntType
+		}
+	case "number":
+		if schema.Format == "double" {
+			return DoubleType
+		} else {
+			return FloatType
+		}
+	case "string":
+		return StringType
+	case "array":
+		return walkArraySchema(schema)
+	case "object":
+		return walkObjectSchema(schema, recordName)
+	default:
+		// unknown type
+		return nil
 	}
-	b.WriteString("\n")
+}
 
-	// write out the properties
-	for propName, propSchema := range schema.Properties {
-		// write the property name
-		b.WriteString(strings.Repeat(" ", indent+2))
-		b.WriteString(propName)
-		b.WriteString(": ")
-
-		// write the property schema
-		switch propSchema := propSchema.(type) {
-		case *SchemaObject:
-			b.WriteString("\n")
-			b.WriteString(displaySchema(propSchema, indent+4))
-		case *SchemaArray:
-			b.WriteString(fmt.Sprintf("%sItems: %s\n", indent, propSchema.Items.Type))
-		case *SchemaNumber:
-			b.WriteString(fmt.Sprintf("Number (%v to %v)\n", propSchema.Minimum, propSchema.Maximum))
-		case *SchemaString:
-			b.WriteString(fmt.Sprintf("String (%d to %d characters)\n", propSchema.MinLength, propSchema.MaxLength))
+func walkArraySchema(schema *JSONSchema) AvroSchema {
+	if schema.Items != nil {
+		return &ArrayType{
+			Type:  "array",
+			Items: walkSchema(schema.Items, ""),
 		}
 	}
-
-	return b.String()
-}
-
-func toStringSlice(input []interface{}) []string {
-	result := make([]string, len(input))
-	for i, v := range input {
-		result[i] = v.(string)
+	// If Items is not set, return an array of null and any type
+	var any AnyType
+	return &UnionType{
+		Types: []AvroSchema{
+			NullType,
+			&any,
+		},
 	}
-	return result
 }
 
-func getSchemaType(obj map[string]interface{}) interface{} {
-	if val, ok := obj["type"]; ok {
-		switch val := val.(type) {
-		case string:
-			return val
-		case []interface{}:
-			types := make([]string, len(val))
-			for i, t := range val {
-				types[i] = t.(string)
+func walkObjectSchema(schema *JSONSchema, recordName string) AvroSchema {
+	fields := make([]RecordField, 0, len(schema.Properties))
+	for name, prop := range schema.Properties {
+		isRequired := false
+		for _, requiredProp := range schema.Required {
+			if requiredProp == name {
+				isRequired = true
+				break
 			}
-			return types
 		}
-	}
-	return nil
-}
 
-func getSchemaEnum(obj map[string]interface{}) []interface{} {
-	if val, ok := obj["enum"]; ok {
-		if enums, ok := val.([]interface{}); ok {
-			result := make([]interface{}, len(enums))
-			for i, e := range enums {
-				result[i] = e
+		// Check if additional properties are allowed
+		if schema.AdditionalProperties != nil &&
+			schema.AdditionalProperties.Type == "boolean" &&
+			schema.AdditionalProperties.Enum != nil &&
+			schema.AdditionalProperties.Enum[0] == false {
+			if !isRequired {
+				continue
 			}
-			return result
 		}
-	}
 
-	return nil
-}
+		var fieldType AvroSchema
 
-func getSchemaConst(obj map[string]interface{}) interface{} {
-	if val, ok := obj["const"]; ok {
-		return val
-	}
-	return nil
-}
-
-func getSchemaDescription(obj map[string]interface{}) interface{} {
-	if val, ok := obj["description"]; ok {
-		return val.(string)
-	}
-	return nil
-}
-
-func populateGeneral(obj map[string]interface{}, schema interface{}) {
-
-	if t := getSchemaType(obj); t != nil {
-		reflect.ValueOf(schema).Elem().FieldByName("Type").Set(reflect.ValueOf(t))
-	}
-	if enums := getSchemaEnum(obj); enums != nil {
-		reflect.ValueOf(schema).Elem().FieldByName("Enum").Set(reflect.ValueOf(enums))
-	}
-	if c := getSchemaConst(obj); c != nil {
-		reflect.ValueOf(schema).Elem().FieldByName("Const").Set(reflect.ValueOf(c))
-	}
-	if d := getSchemaDescription(obj); d != nil {
-		reflect.ValueOf(schema).Elem().FieldByName("Description").Set(reflect.ValueOf(d))
-	}
-}
-
-func populateSchemaObject(obj map[string]interface{}, schema *SchemaObject) {
-
-	populateGeneral(obj, schema)
-
-	if val, ok := obj["$id"]; ok {
-		schema.Id = val.(string)
-	}
-	if val, ok := obj["$schema"]; ok {
-		schema.Schema = val.(string)
-	}
-	if val, ok := obj["title"]; ok {
-		schema.Title = val.(string)
-	}
-	if val, ok := obj["properties"]; ok {
-		schema.Properties = val.(map[string]interface{})
-	}
-	if val, ok := obj["required"]; ok {
-		schema.Required = toStringSlice(val.([]interface{}))
-	}
-	if val, ok := obj["maxProperties"]; ok {
-		schema.MaxProperties = uint64(val.(float64))
-	}
-
-	if val, ok := obj["minProperties"]; ok {
-		schema.MinProperties = uint64(val.(float64))
-	}
-}
-
-func populateSchemaArrayItems(obj map[string]interface{}, schema *SchemaArrayItems) {
-	if items, ok := obj["items"].(map[string]interface{}); ok {
-		if itemType, ok := items["type"].(string); ok {
-			schema.Type = itemType
+		if isRequired {
+			fieldType = walkSchema(prop, name)
+		} else {
+			fieldType = &UnionType{
+				Types: []AvroSchema{
+					walkSchema(prop, name),
+					NullType,
+				},
+			}
 		}
-	}
-}
 
-func populateSchemaArray(obj map[string]interface{}, schema *SchemaArray) {
-
-	populateGeneral(obj, schema)
-	schemaItems := &SchemaArrayItems{}
-	populateSchemaArrayItems(obj, schemaItems)
-	schema.Items = schemaItems
-
-	if v, ok := obj["minItems"]; ok {
-		schema.MinItems = uint64(v.(float64))
-	}
-	if v, ok := obj["maxItems"]; ok {
-		schema.MaxItems = uint64(v.(float64))
-	}
-	if v, ok := obj["maxContains"]; ok {
-		schema.MaxContains = uint64(v.(float64))
-	}
-	if v, ok := obj["minContains"]; ok {
-		schema.MinContains = uint64(v.(float64))
-	}
-	if v, ok := obj["uniqueItems"]; ok {
-		schema.UniqueItems = v.(bool)
-	}
-}
-
-func populateSchemaNumber(obj map[string]interface{}, schema *SchemaNumber) {
-
-	populateGeneral(obj, schema)
-
-	if v, ok := obj["multipleOf"].(float64); ok {
-		schema.MultipleOf = v
-	}
-	if v, ok := obj["minimum"].(float64); ok {
-		schema.Minimum = v
-	}
-	if v, ok := obj["exclusiveMinimum"].(float64); ok {
-		schema.ExclusiveMinimum = v
-	}
-	if v, ok := obj["maximum"].(float64); ok {
-		schema.Maximum = v
-	}
-	if v, ok := obj["exclusiveMaximum"].(float64); ok {
-		schema.ExclusiveMaximum = v
-	}
-}
-
-func populateSchemaString(obj map[string]interface{}, schema *SchemaString) {
-
-	populateGeneral(obj, schema)
-
-	if val, ok := obj["pattern"]; ok {
-		schema.Pattern = val.(string)
-	}
-	if val, ok := obj["maxLength"]; ok {
-		schema.MaxLength = uint64(val.(float64))
-	}
-	if val, ok := obj["minLength"]; ok {
-		schema.MinLength = uint64(val.(float64))
-	}
-}
-
-func walkProperties(properties map[string]interface{}, schema *SchemaObject) {
-	for name, property := range properties {
-		propMap := property.(map[string]interface{})
-		propType := propMap["type"].(string)
-
-		fullName := name
-		switch propType {
-		case "object":
-			propertySchema := &SchemaObject{}
-			populateSchemaObject(propMap, propertySchema)
-			schema.Properties[name] = propertySchema
-			walkProperties(propertySchema.Properties, propertySchema)
-		case "array":
-			propertySchema := &SchemaArray{}
-			populateSchemaArray(propMap, propertySchema)
-			schema.Properties[name] = propertySchema
-		case "number":
-			propertySchema := &SchemaNumber{}
-			populateSchemaNumber(propMap, propertySchema)
-			schema.Properties[name] = propertySchema
-		case "string":
-			propertySchema := &SchemaString{}
-			populateSchemaString(propMap, propertySchema)
-			schema.Properties[name] = propertySchema
-		default:
-			fmt.Printf("unknown %s: %s\n", fullName, propType)
+		field := RecordField{
+			Name: name,
+			Type: fieldType,
 		}
-	}
-}
-
-///avro
-// TODO: the function should accept any start point
-// TODO: check which properties for json-schema should be added
-// TODO: implement Union "null"
-// TODO: what about defaults?
-// TODO: split up function it is too big
-
-func buildAvroSchema(obj *SchemaObject) (interface{}, error) {
-	schema := make(map[string]interface{})
-	schema["type"] = "record"
-	schema["name"] = obj.Title
-	schema["namespace"] = obj.Title
-
-	if obj.Description != "" {
-		schema["doc"] = obj.Description
-	}
-
-	fields := make([]map[string]interface{}, 0)
-
-	for key, value := range obj.Properties {
-		field := make(map[string]interface{})
-		field["name"] = key
-
-		switch v := value.(type) {
-		case *SchemaObject:
-			v.Title = fmt.Sprintf("%srecord", key)
-			fieldType, err := buildAvroSchema(v)
-			if err != nil {
-				return "", err
-			}
-			field["type"] = fieldType
-			if v.Description != "" {
-				field["doc"] = v.Description
-			}
-		case *SchemaArray:
-			field["type"] = map[string]interface{}{
-				"type":  "array",
-				"items": v.Items.Type,
-			}
-			if v.Description != "" {
-				field["doc"] = v.Description
-			}
-		case *SchemaNumber:
-			switch v.Type {
-			case "integer":
-				field["type"] = "int"
-			case "number":
-				field["type"] = "float"
-			}
-			if v.Description != "" {
-				field["doc"] = v.Description
-			}
-		case *SchemaString:
-			field["type"] = "string"
-			if v.Description != "" {
-				field["doc"] = v.Description
-			}
-		default:
-			return "", fmt.Errorf("unknown schema type: %T", v)
+		if prop.Default != nil {
+			field.DefaultValue = prop.Default
+		}
+		if prop.Description != "" {
+			field.Doc = prop.Description
 		}
 		fields = append(fields, field)
 	}
+	if schema.Title == "" {
+		schema.Title = fmt.Sprintf("%srecord", recordName)
+	}
+	return &RecordType{
+		Type:   "record",
+		Name:   schema.Title,
+		Fields: fields,
+	}
+}
 
-	schema["fields"] = fields
+func jsonSchemaToAvroSchema(schemaJSON []byte) (AvroSchema, error) {
+	schema := &JSONSchema{}
+	err := json.Unmarshal(schemaJSON, schema)
+	if err != nil {
+		return nil, err
+	}
 
-	return schema, nil
+	return walkSchema(schema, ""), nil
 }
---
+++ readme.md
@@ -0,0 +1,26 @@
+Schema Registry
+=========================
+Simple Schema Registry. Stores JSON-schemas in a MongoDB. 
+
+
+# Endpoints
+------------
+GET /schemas/<name>
+GET /schemas/<name>/avro
+GET /schemas/<name>/<version>
+POST /schemas/<name>    
+PUT /schemas/<name>
+
+# Avro
+## Specs
+https://avro.apache.org/docs/1.11.1/specification/
+
+## Schema validation
+http://avro.tarantool.org/#
+
+# Json
+## Specs
+https://json-schema.org/
+
+## Schema validation
+https://www.jsonschemavalidator.net/
